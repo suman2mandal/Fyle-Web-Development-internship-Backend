8ef0c3bae7155025499f3d4f614daa6914bb51bc
Suman Mandal <36975298+suman2mandal@users.noreply.github.com>
2024-01-18 01:42:38 +0530

    Cors error fix

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ const axiosInstance = axios.create({
     }
 });
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send('Hello World!🚀');
 });
